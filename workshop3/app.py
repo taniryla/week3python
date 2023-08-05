927633a3d8accfd1329d68e80ae5577d796f4045
Chako <ryland0000@gmail.com>
2023-08-04 22:14:57 -0700

    moved variables to app

---
+++ workshop3/app.py
@@ -0,0 +1,12 @@
+from donations_pkg.homepage import show_homepage
+
+database = {"admin": "password123"}
+donations = []
+authorized_user = ""
+
+show_homepage()
+
+if authorized_user == "":
+    print("You must be logged in to donate.")
+else:
+    print(f"Logged in as: {authorized_user}")
--- workshop3/donation_pkg/__init__.py
+++
@@ -1,7 +0,0 @@
-from donation_pkg.homepage import show_homepage
-
-database = {"admin": "password123"}
-donations = []
-authorized_user = ""
-
-show_homepage()
